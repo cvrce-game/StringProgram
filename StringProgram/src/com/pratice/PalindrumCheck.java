c42f2833427edc84d6868eea9dcd8a83f1598173
Papun Kumar Mohapatra <papmohapatra@deloitte.com>
2025-06-27 10:15:56 +0530

    PalindrumCheck

--- StringProgram/src/com/pratice/PalindrumCheck.java
+++ StringProgram/src/com/pratice/PalindrumCheck.java
@@ -1,23 +1,22 @@
 package com.pratice;
 
-import java.util.Arrays;
 
 public class PalindrumCheck {
-
 	public static void main(String[] args) {
 
-		String input = "Madam";
+		String input = "1221";
+		String lowerInput = input.toLowerCase();
 
-		for (int i = 0; i < input.length() / 2; i++) {
-			char startChar = Character.toLowerCase(input.charAt(i));
-			char endChar = Character.toLowerCase(input.charAt(input.length() - 1 - i));
+		for (int i = 0; i < lowerInput.length() / 2; i++) {
+			char startChar = lowerInput.charAt(i);
+			char endChar = lowerInput.charAt(lowerInput.length() - 1 - i);
 
 			if (startChar != endChar) {
-				System.out.println(input + " is Not a Palindrome");
+				 System.out.println(input + " is not a Palindrome");
 				return;
 			}
 		}
-		System.out.println(input + " is Not a Palindrome");
+		System.out.println(input + " is a Palindrome");
 	}
 
 }
